457046b6aa436314c51441a15b2dcef8c9278ce6
maarirods <rodspersonal22@gmail.com>
2021-08-12 20:56:36 -0300

    Completei o script de incluirPet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -10,6 +10,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
@@ -40,6 +43,10 @@ public class Pet {
         .then() // Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Bolina"))
+                .body("status", is("available"))
+                .body("category.name", is("cat"))
+                .body("tags.name", contains("sta"))
         ;
 
 
